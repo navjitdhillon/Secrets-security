af1a94f16af69c314b9c4b72e517b104c4a44fbd
unknown <Navjit Dhillon@DESKTOP-KRO4E60.branttel.com>
2020-10-22 22:15:04 -0400

    level 1: username and password

---
+++ app.js
@@ -0,0 +1,67 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const userSchema = {
+  username: String,
+  password: String
+}
+
+const User = mongoose.model("User", userSchema);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.route("/login")
+  .get(function(req, res){
+    res.render("login");
+  })
+  .post(function(req, res){
+    User.findOne({username: req.body.username}, function(err, result){
+      if(!err){
+        if(!result){
+          console.log("user not found");
+          res.redirect("/");
+        } else {
+          if(req.body.password===result.password){
+            res.render("secrets");
+          } else {
+            console.log("password incorrect");
+            res.redirect("/");
+          }
+        }
+      }
+    });
+  });
+
+app.route("/register")
+  .get(function(req, res){
+    res.render("register");
+  })
+  .post(function(req, res){
+    const newUser = new User({
+      username:req.body.username,
+      password: req.body.password
+    });
+    newUser.save(function(err, result){
+      if(!err){
+        console.log(result);
+        res.render("secrets");
+      }
+    });
+  });
+
+app.listen(3000, function(){
+  console.log("Server is listening on port 3000...");
+});
