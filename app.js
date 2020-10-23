d8574fda2396550f9e740d3310e501f8532247b8
unknown <Navjit Dhillon@DESKTOP-KRO4E60.branttel.com>
2020-10-23 00:44:36 -0400

    level 2: apply aes256 encryption to passwords saved in mongo db

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   username: String,
   password: String
-}
+});
+
+const secret = "mystonglittleunguessablesecret";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
