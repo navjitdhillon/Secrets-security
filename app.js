09d4f1065ee78e842213c9ca74c3caae12a41897
unknown <Navjit Dhillon@DESKTOP-KRO4E60.branttel.com>
2020-10-27 01:21:24 -0400

    implement passport based cookies and session authorization

--- app.js
+++ app.js
@@ -1,39 +1,68 @@
-//jshint esversion:6
-// only when using common encryption, or other environment variables
-// require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-//when using common encryption instead of md5 or bcrypt hash
-// const encrypt = require("mongoose-encryption");
 
-// when using md5 instead of a beter bcrypt alternative
-// const md5 = require("md5");
+// Require 3 packages for local authentication using passport
+const session = require("express-session");
+const passport = require("passport");
+  // was installed through npm but no need to require passport-local (for local strategy)
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
 
 const app = express();
-
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
-
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String
-});
-
-// updated to using md5 hash instead of encryption
-// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
-
-const User = mongoose.model("User", userSchema);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// initialize express-session module to use the sessions with express
+app.use(session({
+  secret: "my trusty little secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+// initialize passport and configure it to manage express sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// set up or connect to the DB, create the schema as usual
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+// setup userSchema to use passportLocalMongoose as a plugin, to use passport to hash, salt and save credentials to mongoDB
+userSchema.plugin(passportLocalMongoose);
+
+// create the model from schema as usual
+const User = mongoose.model("User", userSchema);
+
+// use createStrategy method to setup passport-local LocalStrategy with the correct options.
+passport.use(User.createStrategy());
+
+// setup passport to serialize and deserialize the user model for session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// app routes =>
 app.get("/", function(req, res){
   res.render("home");
+});
+
+app.get("/secrets", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }else{
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
 });
 
 app.route("/login")
@@ -41,24 +70,15 @@ app.route("/login")
     res.render("login");
   })
   .post(function(req, res){
-    User.findOne({username: req.body.username}, function(err, result){
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+    req.login(user, function(err, result){
       if(!err){
-        if(!result){
-          console.log("user not found");
-          res.redirect("/");
-        } else {
-          bcrypt.compare(req.body.password, result.password, function(err, result) {
-            if(!err){
-              if(result==true){
-                res.render("secrets");
-                console.log("Its a match!");
-              } else {
-                console.log("Password incorrect");
-                res.redirect("/");
-              }
-            }
-          });
-        }
+        passport.authenticate("local")( req, res, function(){
+        res.redirect("/secrets");
+        });
       }
     });
   });
@@ -68,17 +88,15 @@ app.route("/register")
     res.render("register");
   })
   .post(function(req, res){
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-      const newUser = new User({
-        username:req.body.username,
-        password:hash
-      });
-      newUser.save(function(err, result){
-        if(!err){
-          console.log(result);
-          res.render("secrets");
-        }
-      });
+    User.register({username:req.body.username}, req.body.password, function(err, user) {
+    // User.save functionality inbuilt in .register function of passportLocalMongoose package.
+      if(!err){
+        // for continous session logged-in
+        passport.authenticate("local")(req, res, function(){
+          res.redirect("/secrets");
+        });
+        console.log(user);
+      }
     });
   });
 
