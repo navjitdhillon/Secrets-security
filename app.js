e346937c2fca9a0f59929a9c83b31003250c4d2c
unknown <Navjit Dhillon@DESKTOP-KRO4E60.branttel.com>
2020-10-28 18:51:38 -0400

    level 6: add Google Oauth authentication for registration and login

--- app.js
+++ app.js
@@ -2,40 +2,49 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-// Require 3 packages for local authentication using passport
+// Require 3 packages for local authentication using passport =>
 const session = require("express-session");
 const passport = require("passport");
-  // was installed through npm but no need to require passport-local (for local strategy)
+  // although installed through npm but no need to require passport-local (for local strategy)
 const passportLocalMongoose = require("passport-local-mongoose");
 
+// for google oauth 2.0 passport LocalStrategy
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
+// plugin for Mongoose which adds a findOrCreate method to models (as needed in passport google oauth documentation).
+const findOrCreate = require("mongoose-findorcreate");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-// initialize express-session module to use the sessions with express
+// initialize express-session module to use the sessions with express =>
 app.use(session({
   secret: "my trusty little secret",
   resave: false,
   saveUninitialized: false
 }));
 
-// initialize passport and configure it to manage express sessions
+// initialize passport and configure it to manage express sessions =>
 app.use(passport.initialize());
 app.use(passport.session());
 
-// set up or connect to the DB, create the schema as usual
+// set up or connect to the DB, create the schema as usual =>
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 const userSchema = new mongoose.Schema({
   username: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
-// setup userSchema to use passportLocalMongoose as a plugin, to use passport to hash, salt and save credentials to mongoDB
+// add passportLocalMongoose plugin for userSchema to use passport to hash, salt and save credentials to mongoDB
 userSchema.plugin(passportLocalMongoose);
+
+// add findOrCreate plugin to userSchema to provide findOrCreate functionality needed in passport's google auth strategy
+userSchema.plugin(findOrCreate);
 
 // create the model from schema as usual
 const User = mongoose.model("User", userSchema);
@@ -44,8 +53,31 @@ const User = mongoose.model("User", userSchema);
 passport.use(User.createStrategy());
 
 // setup passport to serialize and deserialize the user model for session support
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// shortcuts used for local strategy will no longer work for google etc.
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+// google oauth 2.0 passport strategy setup =>
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+    // this callbackURL field above needs to match the redirect uri field in google developer console
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 // app routes =>
 app.get("/", function(req, res){
@@ -64,6 +96,16 @@ app.get("/logout", function(req, res){
   req.logout();
   res.redirect("/");
 });
+
+// get routes for google auth and redirect options on register and login pages =>
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] }));
+
+app.get("/auth/google/callback",
+   passport.authenticate('google', { failureRedirect: '/login' }),
+   function(req, res){
+     res.redirect("/secrets");
+   });
 
 app.route("/login")
   .get(function(req, res){
